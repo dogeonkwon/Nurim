44c4976dc4b9948209b8c14bbf32af23e69adbb9
공지훈 <wlgns3914@naver.com>
2022-09-06 18:12:08 +0900

    [S07P22E105-53] feat:back: spring boot 프로젝트 생성

---
+++ backend/src/main/java/com/bigdata/WheelKo/WheelKoApplication.java
@@ -0,0 +1,13 @@
+package com.bigdata.WheelKo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheelKoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheelKoApplication.class, args);
+	}
+
+}

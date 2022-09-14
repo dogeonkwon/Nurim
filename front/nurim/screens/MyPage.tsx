2bd8542362c27298d7dc0db946dfafa77de1ce70
김국진 <rnrwls0211@naver.com>
2022-09-14 17:46:16 +0900

    fix:front:MyPageNotExport오류수정

--- front/nurim/screens/MyPage.tsx
+++ front/nurim/screens/MyPage.tsx
@@ -1,1 +1,12 @@
-export {};
+import {View, Text} from 'react-native';
+
+interface Props {}
+const MyPage = ({}: Props) => {
+  return (
+    <View>
+      <Text>hi</Text>
+    </View>
+  );
+};
+
+export default MyPage;

b2177a4d2f6ff58cfe6091e8392c09d99465f249
김국진 <rnrwls0211@naver.com>
2022-09-22 13:54:58 +0900

    [S07P22E105-189]feat:front:네이버로그인Android

--- front/nurim/components/LogOutSideBar/index.tsx
+++ front/nurim/components/LogOutSideBar/index.tsx
@@ -1,13 +1,14 @@
 // 로그인 상태의 사이드바 프로필
 // 2022.09.15 김국진
 import React from 'react';
-import {View, ScrollView, StyleSheet} from 'react-native';
+import {View, ScrollView, StyleSheet, Platform} from 'react-native';
 //import styled from 'styled-components/native';
 import {styled} from '@mui/styles';
 import {Button, Text, Avatar, Divider, Icon} from '@rneui/themed';
 import {getFont} from '../../common/font';
 import {getColor} from '../../common/colors';
 import {signInWithKakao} from '../../modules/kakao';
+import {naverLogin} from '../../modules/naver';
 
 const styles = StyleSheet.create({
   Divider: {
@@ -22,66 +23,56 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     overflow: 'hidden',
   },
-  Button: {
-    kakaoButton: {
-      backgroundColor: getColor('KAKAO'),
-    },
-    naverButton: {
-      backgroundColor: getColor('NAVER'),
-    },
-    ButtonContainer: {
-      height: 50,
-      width: 240,
-      marginHorizontal: 1,
-      marginVertical: 5,
-    },
+  kakaoButton: {
+    backgroundColor: getColor('KAKAO'),
   },
-  Text: {
-    nameText: {
-      marginTop: 15,
-      marginBottom: 15,
-      fontSize: 22,
-      fontFamily: getFont(3),
-    },
-    ButtonText: {
-      fontSize: 18,
-      marginRight: 50,
-      paddingLeft: 40,
-    },
-    kakaoButtomText: {
-      color: 'black',
-    },
-    naverButtonText: {
-      color: 'white',
-    },
+  naverButton: {
+    backgroundColor: getColor('NAVER'),
+  },
+  ButtonContainer: {
+    height: 50,
+    width: 240,
+    marginHorizontal: 1,
+    marginVertical: 5,
+  },
+  nameText: {
+    marginTop: 15,
+    marginBottom: 15,
+    fontSize: 22,
+    fontFamily: getFont(3),
+  },
+  ButtonText: {
+    fontSize: 18,
+    marginRight: 50,
+    paddingLeft: 40,
+  },
+  kakaoButtomText: {
+    color: 'black',
+  },
+  naverButtonText: {
+    color: 'white',
   },
 });
 
 const LogOutSideBar = () => {
   return (
-    <Divider style={styles.Divider} fill center spacing={4}>
-      <Text style={styles.Text.nameText}>로그인 해주세요.</Text>
+    <Divider style={styles.Divider}>
+      <Text style={styles.nameText}>로그인 해주세요.</Text>
       <Button
-        buttonStyle={styles.Button.kakaoButton}
-        containerStyle={styles.Button.ButtonContainer}
+        buttonStyle={styles.kakaoButton}
+        containerStyle={styles.ButtonContainer}
         onPress={() => signInWithKakao()}>
-        <Avatar
-          source={require('../../assets/images/KAKAO_LOGO_EDGE.png')}
-          name="home"
-          color="white"
-        />
-        <Text style={[styles.Text.kakaoButtomText, styles.Text.ButtonText]}>
+        <Avatar source={require('../../assets/images/KAKAO_LOGO_EDGE.png')} />
+        <Text style={[styles.kakaoButtomText, styles.ButtonText]}>
           카카오 로그인
         </Text>
       </Button>
       <Button
-        buttonStyle={styles.Button.naverButton}
-        containerStyle={[styles.Button.ButtonContainer, {marginBottom: 20}]}>
-        <Avatar
-          source={require('../../assets/images/NAVER_LOGO.png')}
-          color="white"
-        />
-        <Text style={[styles.Text.naverButtonText, styles.Text.ButtonText]}>
+        buttonStyle={styles.naverButton}
+        containerStyle={[styles.ButtonContainer, {marginBottom: 20}]}
+        onPress={() => naverLogin(Platform.OS)}>
+        <Avatar source={require('../../assets/images/NAVER_LOGO.png')} />
+        <Text style={[styles.naverButtonText, styles.ButtonText]}>
           네이버 로그인
         </Text>
       </Button>

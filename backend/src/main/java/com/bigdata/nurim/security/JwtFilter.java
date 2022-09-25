b47f8f236931f02c25f9c86bbf354d54bcb3bbe2
공지훈 <wlgns3914@naver.com>
2022-09-26 02:40:36 +0900

    [S07P22E105-178]feat:back:Apply custom filter

--- backend/src/main/java/com/bigdata/nurim/security/JwtFilter.java
+++ backend/src/main/java/com/bigdata/nurim/security/JwtFilter.java
@@ -1,5 +1,8 @@
 package com.bigdata.nurim.security;
 
+import com.bigdata.nurim.Exception.UserNotFoundException;
+import com.bigdata.nurim.entity.User;
+import com.bigdata.nurim.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
@@ -13,17 +16,21 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.Optional;
 
 public class JwtFilter extends GenericFilterBean {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtFilter.class);
 
-    public static final String AUTHORIZATION_HEADER = "Authorization";
+    private final UserRepository userRepository;
 
-    private TokenProvider tokenProvider;
+    public static final String AUTHORIZATION_HEADER = "jwt-token";
 
-    public JwtFilter(TokenProvider tokenProvider) {
+    private final TokenProvider tokenProvider;
+
+    public JwtFilter(TokenProvider tokenProvider,UserRepository userRepository) {
         this.tokenProvider = tokenProvider;
+        this.userRepository = userRepository;
     }
 
     @Override // 실제 필터링 로직으로, JWT 토큰의 인증정보를 현재 실행중인 SecurityContext에 저장하는 역할 수행
@@ -35,11 +42,17 @@ public class JwtFilter extends GenericFilterBean {
         String requestURI = httpServletRequest.getRequestURI();
 
         // 토큰의 유효성을 검사하고 (validateToken(jwt))
-        if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+        if (jwt!=null&&StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
             // 정상이면 Authentication 객체를 받아와서
             Authentication authentication = tokenProvider.getAuthentication(jwt);
             // SecurityContext에 set 해준다.
             SecurityContextHolder.getContext().setAuthentication(authentication);
+            String email = (String) authentication.getPrincipal();
+
+            Optional<User> user = userRepository.findByEmail(email);
+            System.out.println(email+" "+user.toString());
+            if (!user.isPresent()) throw new UserNotFoundException();
+
             logger.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
         } else {
             logger.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
@@ -51,9 +64,14 @@ public class JwtFilter extends GenericFilterBean {
     // Request Header에서 토큰 정보를 꺼내오는 메서드
     private String resolveToken(HttpServletRequest request) {
         String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+//        if (StringUtils.hasText(bearerToken)) {
+//            return bearerToken;
+//        }
+
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
             return bearerToken.substring(7);
         }
+
         return null;
     }
 }

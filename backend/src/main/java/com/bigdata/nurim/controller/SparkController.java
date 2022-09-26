c8b9b985757a19c2eac953e87109a2165d5537c9
정찬우 <jeong_2240@naver.com>
2022-09-26 17:53:23 +0900

    [S07P22E105-217]feat:back:Komoran 환경설정

--- backend/src/main/java/com/bigdata/nurim/controller/SparkController.java
+++ backend/src/main/java/com/bigdata/nurim/controller/SparkController.java
@@ -1,7 +1,12 @@
 package com.bigdata.nurim.controller;
 
 import com.bigdata.nurim.service.SparkService;
+import kr.co.shineware.nlp.komoran.constant.DEFAULT_MODEL;
+import kr.co.shineware.nlp.komoran.core.Komoran;
+import kr.co.shineware.nlp.komoran.model.KomoranResult;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,13 +17,33 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@Slf4j
 public class SparkController {
 
     @Autowired
     SparkService service;
 
+
+    @Value("${komoran.path}")
+    String userDicPath;
+
     @RequestMapping(method = RequestMethod.POST, path = "/wordcount")
     public Map<String, Long> count(@RequestParam(value = "content",required = false) String words) {
+
+
+        Komoran komoran = new Komoran(DEFAULT_MODEL.LIGHT);
+        komoran.setUserDic(userDicPath);
+
+        System.out.print("Plane Text: ");
+        System.out.println(words);
+
+        KomoranResult analyzeResultList = komoran.analyze(words);
+
+        System.out.println("getNouns : "+analyzeResultList.getNouns());
+        System.out.println("getPlaneText : "+analyzeResultList.getPlainText());
+        System.out.println("getList : "+analyzeResultList.getList());
+        System.out.println("getMorphesByTags : "+analyzeResultList.getMorphesByTags("NP", "NNP", "JKB"));
+
         List<String> wordList = Arrays.asList(words.split(","));
         return service.getCount(wordList);
     }

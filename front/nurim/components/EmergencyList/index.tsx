7463831c82ecc0127bc55b8c7477fe197c9274a2
권도건 <rnsjquddbs@gmail.com>
2022-09-28 15:48:49 +0900

    [S07P22E105-157]feat:front: 필터바 클릭 시 마커 표시

--- front/nurim/components/EmergencyList/index.tsx
+++ front/nurim/components/EmergencyList/index.tsx
@@ -1,8 +1,14 @@
+<<<<<<< Updated upstream
 import React, { useState } from 'react';
 import { Button, Dialog, ListItem, Avatar, Icon } from '@rneui/themed';
 import { View, StyleSheet, Linking } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+=======
+import React, {useState} from 'react';
+import {Button, Dialog, ListItem, Avatar} from '@rneui/themed';
+import {View, StyleSheet, Linking} from 'react-native';
+>>>>>>> Stashed changes
 type DialogComponentProps = {};
 
 const EmergencyList: React.FunctionComponent<DialogComponentProps> = () => {
@@ -21,32 +27,34 @@ const EmergencyList: React.FunctionComponent<DialogComponentProps> = () => {
     {
       name: '119',
       subTitle: '근처 소방서로 연락',
-      phoneNumber: '119'
+      phoneNumber: '119',
     },
     {
       name: '112',
       subTitle: '근처 경찰서로 연락',
-      phoneNumber: '112'
+      phoneNumber: '112',
     },
   ];
   //상태변화에 따라 재할당이 필요하기에 재할당이 가능한 let 사용
-  let loginButton = '로그인하기'
-  let buttonEvent = () => {setLogin(true)};
+  let loginButton = '로그인하기';
+  let buttonEvent = () => {
+    setLogin(true);
+  };
 
   //로그인시에 이하를 적용
   if (login == true) {
     //버튼의 기능을 로그아웃으로 변경
-    buttonEvent = () => {setLogin(false)}
+    buttonEvent = () => {
+      setLogin(false);
+    };
     //버튼 타이틀 변경
-    loginButton = '로그아웃하기'
+    loginButton = '로그아웃하기';
     //재할당이 불가능한 const이기에 .push로 이하를 리스트 마지막에 추가
-    emergencyCallList.push(
-      {
-        name: '비상연락처',
-        subTitle: '미리 지정해 둔 번호로 연락',
-        phoneNumber: '01092403692'
-      },
-    );
+    emergencyCallList.push({
+      name: '비상연락처',
+      subTitle: '미리 지정해 둔 번호로 연락',
+      phoneNumber: '01092403692',
+    });
   }
 
   return (
@@ -62,6 +70,7 @@ const EmergencyList: React.FunctionComponent<DialogComponentProps> = () => {
           title="비상 연락"
           onPress={toggleDialog}
           buttonStyle={styles.button}
+<<<<<<< Updated upstream
         /> */}
         <Button 
           title={loginButton} //로그인상태에 따라 버튼타이틀이 변화
@@ -71,20 +80,31 @@ const EmergencyList: React.FunctionComponent<DialogComponentProps> = () => {
           <Dialog
             isVisible={visible}
             onBackdropPress={toggleDialog}
+=======
+        />
+        <Button
+          title={loginButton} //로그인상태에 따라 버튼타이틀이 변화
+          onPress={buttonEvent} //로그인상태에 따라 클릭시 실행되는 함수가 변화
+        />
+      </View>
+      <Dialog isVisible={visible} onBackdropPress={toggleDialog}>
+        <Dialog.Title title="비상 연락" />
+        {emergencyCallList.map((l, i) => (
+          <ListItem
+            key={i}
+            containerStyle={{
+              marginHorizontal: -10,
+              borderRadius: 8,
+            }}
+            onPress={() => {
+              Linking.openURL(`tel:${l.phoneNumber}`);
+            }} //리스트의 phoneNumber 연락
+>>>>>>> Stashed changes
           >
-            <Dialog.Title title="비상 연락"/>
-            {emergencyCallList.map((l, i) => (
-            <ListItem
-              key={i}
-              containerStyle={{
-                marginHorizontal: -10,
-                borderRadius: 8,
-              }}
-              onPress={()=>{Linking.openURL(`tel:${l.phoneNumber}`)}}//리스트의 phoneNumber 연락
-            >
-          <Avatar source={require('../../assets/images/PHONE_CALL.png')} />
+            <Avatar source={require('../../assets/images/PHONE_CALL.png')} />
             <ListItem.Content>
-              <ListItem.Title style={{ color: 'red', fontSize: 25, fontWeight: 'bold' }}>
+              <ListItem.Title
+                style={{color: 'red', fontSize: 25, fontWeight: 'bold'}}>
                 {l.name}
               </ListItem.Title>
               <ListItem.Subtitle>{l.subTitle}</ListItem.Subtitle>
@@ -97,6 +117,7 @@ const EmergencyList: React.FunctionComponent<DialogComponentProps> = () => {
 };
 
 const styles = StyleSheet.create({
+<<<<<<< Updated upstream
 button: {
   borderRadius: 6,
   width: 220,
@@ -105,6 +126,18 @@ button: {
 iconContainer: {
   marginTop : -305
 },
+=======
+  button: {
+    borderRadius: 6,
+    width: 220,
+    margin: 20,
+  },
+  buttonContainer: {
+    margin: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+>>>>>>> Stashed changes
 });
 
 export default EmergencyList;

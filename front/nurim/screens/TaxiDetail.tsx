a86939e8d7590d811343e39605492fe7abf47c74
이태엽 <j90310@naver.com>
2022-09-29 16:28:14 +0900

    [S07P22E105-191]Navigator적용하여 하드코딩 단위에서 단위 테스트 완성

--- front/nurim/screens/TaxiDetail.tsx
+++ front/nurim/screens/TaxiDetail.tsx
@@ -1,28 +1,54 @@
 import React, {useState} from 'react';
 import {BottomSheet, Button, ListItem, Image, Text} from '@rneui/themed';
-import {StyleSheet, SafeAreaView} from 'react-native';
+import {StyleSheet, SafeAreaView, ScrollView} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import TaxiInfo from '../components/TaxiInfo';
 import TaxiInfoContent from '../components/TaxiInfoContent';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import PopTab from '../components/PopTab';
 import {MainStackNavigationProp} from './RootStack';
 import {useNavigation} from '@react-navigation/native';
 
 const TaxiDetail = () => {
   const navigation = useNavigation<MainStackNavigationProp>();
+  const AdditionalTaxiInfoContentList = [
+    { title: <Text style={{ backgroundColor: '#36BC9B', color: 'white'}}>하드코딩사항4</Text>,
+    subTitle : <Text>
+    하드코딩사항 {'\n'}
+    하드코딩사항
+    </Text>,
+    },
+    { title: <Text style={{ backgroundColor: '#36BC9B', color: 'white'}}>하드코딩사항5</Text>,
+    subTitle : <Text>
+    하드코딩사항
+    </Text>,
+    },
+    { title: <Text style={{ backgroundColor: '#36BC9B', color: 'white'}}>하드코딩사항6</Text>,
+    subTitle : <Text>
+    하드코딩사항
+    </Text>,
+    },
+];
+
   return (
-    <SafeAreaProvider>
+    <ScrollView>
       <PopTab title="" navigation={navigation} color="" />
-      <ListItem>
-        <ListItem.Content>
-          <TaxiInfo></TaxiInfo>
-          <TaxiInfoContent></TaxiInfoContent>
-        </ListItem.Content>
-      </ListItem>
-    </SafeAreaProvider>
+      <TaxiInfo></TaxiInfo>
+      <TaxiInfoContent></TaxiInfoContent>
+      {AdditionalTaxiInfoContentList.map((l, i) => (
+            <ListItem
+            key={i}
+            //리스트아이템 사이에 줄 추가
+            bottomDivider
+            >
+            <ListItem.Content>
+                <ListItem.Title>{l.title}</ListItem.Title>
+                <ListItem.Subtitle>{l.subTitle}</ListItem.Subtitle>
+            </ListItem.Content>
+            </ListItem>
+        ))}
+    </ScrollView>
   );
 };
+
 
 export default TaxiDetail;

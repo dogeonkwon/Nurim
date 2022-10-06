4a1f35a2d932e428c83dbf795dbface72b304c09
정찬우 <jeong_2240@naver.com>
2022-10-06 21:54:50 +0900

    fix:back: WordCount Delete 요청시 수정

--- front/nurim/ios/SwiftBridge.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SwiftBridge.swift
-//  nurim
-//
-//  Created by kim kukjin on 2022/09/20.
-//
-
-import Foundation
--- front/nurim/screens/MyFavor.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {View} from 'react-native';
-const MyFavor = () => {
-  return <View>즐겨찾기 화면</View>;
-};
-
-export default MyFavor;
